fda4ac1f87631b3f7ec02e3d26cd3d4cf5694559
Jeff-King <szwjf08@gmail.com>
2020-10-08 17:39:41 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,9 @@ struct Seq_info{
 };
 
 // The data reading function
-size_t read_sequence(filesystem_file_data &file_data, void *buffer, size_t offset, size_t length)
+size_t read_sequence(const Travel_altrep_info* altrep_info, void *buffer, size_t offset, size_t length)
 {
-  Seq_info* info = (Seq_info*)file_data.private_data;
+  Seq_info* info = (Seq_info*)altrep_info->private_data;
   for (size_t i = 0; i < length; i++)
   {
     ((double *)buffer)[i] = info -> start + info -> step * (offset + i);
@@ -26,8 +26,13 @@ size_t read_sequence(filesystem_file_data &file_data, void *buffer, size_t offse
 SEXP make_sequence_altrep(size_t n, size_t start, size_t step)
 {
   Seq_info* info = new Seq_info{start, step};
-  SEXP smart_ptr = Rf_protect(Travel_shared_ptr<Seq_info>(info));
-  SEXP x = Rf_protect(Travel_make_altptr(REALSXP, n, read_sequence, info, smart_ptr));
+  Travel_altrep_info altrep_info = {};
+  altrep_info.length = n;
+  altrep_info.type = REALSXP;
+  altrep_info.operations.get_region = read_sequence;
+  altrep_info.private_data = info;
+  altrep_info.protected_data = Rf_protect(Travel_shared_ptr<Seq_info>(info));
+  SEXP x = Rf_protect(Travel_make_altptr(altrep_info));
   Rf_unprotect(2);
   return x;
 }
